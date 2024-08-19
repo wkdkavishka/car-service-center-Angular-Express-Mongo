191470ea9d1213171f15b9d541a380d6aac287d1
wkdkavishka <dkmaxhome@gmail.com>
2024-08-19 21:17:08 +0530

    UPDATE car-card ui

--- car-service-center-Angular-front/src/app/components/other-components/car-card/car-card.component.ts
+++ car-service-center-Angular-front/src/app/components/other-components/car-card/car-card.component.ts
@@ -1,13 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {CarService} from "../../../services/car.service";
 import {Car} from "../../../models/car";
-import {NgIf} from "@angular/common";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-car-card',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    NgClass
   ],
   templateUrl: './car-card.component.html',
   styleUrl: './car-card.component.scss'
@@ -17,14 +18,21 @@ export class CarCardComponent {
   @Input()
   givenCar!: Car; // The selected car from all-cars component
 
-  constructor(private carService: CarService) {
+  selectedTab: string = "stats"; // default tab
 
+  constructor(private carService: CarService) {
   }
 
   onDelete() {
-    this.carService.deleteACar(this.givenCar).then( );
+    this.carService.deleteACar(this.givenCar).then();
   }
 
-  // @Output()
-  // d_index: EventEmitter<number> = new EventEmitter<number>();
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
+
+  selectTabMobile(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedTab = target.value;
+  }
 }
