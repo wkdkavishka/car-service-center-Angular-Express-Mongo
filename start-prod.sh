4f879192955996018efa6bf01be205dc1ff4b6a5
wkdkavishka <w.k.d.kavishka@outlook.com>
2025-02-01 13:49:08 +0530

    FIXes to start script

--- start-prod.sh
+++ start-prod.sh
@@ -9,11 +9,11 @@ tmux kill-session -t $SESSION_NAME 2>/dev/null
 tmux new-session -d -s $SESSION_NAME
 
 # Start Angular frontend in the first pane
-tmux send-keys -t $SESSION_NAME "cd car-service-center-Angular-front && echo 'Starting Angular frontend...' && npm run start-production" C-m
+tmux send-keys -t $SESSION_NAME "cd car-service-center-Angular-front && echo 'Starting Angular frontend...' && npm i && npm run start-prod" C-m
 
 # Split window horizontally for Express backend
 tmux split-window -h -t $SESSION_NAME
-tmux send-keys -t $SESSION_NAME "cd car-service-center-Express-back && echo 'Starting Express backend...' && npm run start-production" C-m
+tmux send-keys -t $SESSION_NAME "cd car-service-center-Express-back && echo 'Starting Express backend...' && npm i && npm run start-prod" C-m
 
 # Attach to session (optional)
 tmux attach -t $SESSION_NAME
